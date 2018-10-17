3de5a8dea5ca567d2edf6ee4d640a808b6aab4c0
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-10-17 00:13:57 -0600

    ahh

--- server.js
+++ server.js
@@ -11,7 +11,18 @@ const sprite = require('./routes/api/sprite');
 const goal = require('./routes/api/goal');
 
 const app = express();
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.use(cors());
+
 //body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -39,14 +50,6 @@ app.use('/api/post', post);
 app.use('/api/sprite', sprite);
 app.use('/api/goal', goal);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
